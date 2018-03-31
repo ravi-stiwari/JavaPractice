b97cbc0a404a9e4a2c9f55849887b528c9dc9245
ravi-stiwari <ravi.rs.tiwari@gmail.com>
2018-04-01 00:20:05 +0530

    Adding first HelloWorld with Multiple Classes in Single File

--- Basics/HelloWorld.java
+++ Basics/HelloWorld.java
@@ -1,7 +1,19 @@
+class ClassA {
+
+    public void method() {
+
+        System.out.println("Method Called of Class A");
+
+    }
+
+}
+
 class HelloWorld {
 
     public static void main(String... args) {
 
+        ClassA classA = new ClassA();
+        classA.method();
         System.out.println("Hello World");
 
     }
